264c08d4dadde853dde10304c3c24bc489f1851f
carolinesdo <71784712+carolinesdo@users.noreply.github.com>
2020-11-23 23:55:37 -0300

    Add files via upload

---
+++ Exercício12.py
@@ -0,0 +1,8 @@
+#ÁREA DO TRIÂNGULO
+
+base = float(input('Digite um valor para a base do triângulo: '))
+altura = float(input('Digite um valor para a altura do triângulo: '))
+
+area_triangulo = (base * altura) / 2
+
+print('A área do triângulo vale: {} (unidades de medida)²'.format(area_triangulo))
