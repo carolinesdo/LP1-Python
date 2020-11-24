901b34ec1859b0e03dc9dc57bd4b135ad677f973
carolinesdo <71784712+carolinesdo@users.noreply.github.com>
2020-11-23 23:46:11 -0300

    Add files via upload

---
+++ Exercício07.py
@@ -0,0 +1,8 @@
+numero = float(input('Digite um número: '))
+
+if numero > 0:
+    print('O numero digitado é posivito!')
+elif numero < 0:
+    print('O número digitado é negativo!')
+else:
+    print('O numero digitado é o elemeno neutro 0.')
